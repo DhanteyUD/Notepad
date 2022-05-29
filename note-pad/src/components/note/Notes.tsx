21b80a8beeb9d5bec9c901cadad2acd71bfab284
DhanteyUD <otseclinton60@gmail.com>
2022-05-29 19:29:16 +0100

    Update

--- note-pad/src/components/note/Notes.tsx
+++ note-pad/src/components/note/Notes.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Container, Button, Card } from 'react-bootstrap';
 import Note from '../../models/note.model';
+import './note.css';
 
 interface INotesProps {
   note: Note;
@@ -14,20 +15,32 @@ const Notes: React.FunctionComponent<INotesProps> = ({
   return (
     <>
       <div className="mb-3">
-        <Card style={{ backgroundColor: note.color }}>
-          <Card.Body>
-            <Card.Title>{note.title}</Card.Title>
-            <Card.Text>{note.content}</Card.Text>
-            <Card.Subtitle className="text-muted">{note.date}</Card.Subtitle>
-            <Button
-              className="mt-3"
-              variant="danger"
-              onClick={() => handleDelete(note.id)}
-            >
-              Delete
-            </Button>
-          </Card.Body>
-        </Card>
+        <Container
+          className="note-container"
+          style={{ backgroundColor: note.color }}
+        >
+          <Card className="card">
+            <Card.Body>
+              <Card.Title>{note.title}</Card.Title>
+              <Card.Text>{note.content}</Card.Text>
+              <Card.Subtitle className="text-muted">{note.date}</Card.Subtitle>
+              <Button
+                className="mt-3 delete"
+                variant="danger"
+                onClick={() => handleDelete(note.id)}
+              >
+                Delete
+              </Button>
+              <Button
+                className="mt-3 edit"
+                variant="secondary"
+                onClick={() => handleDelete(note.id)}
+              >
+                Edit
+              </Button>
+            </Card.Body>
+          </Card>
+        </Container>
       </div>
     </>
   );
